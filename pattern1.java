15ccc84a837cdab7c8f6622b70b7962969288d32
Arnoldaditya17 <as3411643@gmail.com>
2023-05-05 16:41:46 +0530

    first commit

---
+++ pattern1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class pattern1
+{public static void main(String ar[])
+{Scanner sc=new Scanner(System.in);
+int i,j;
+for(i=1;i<=5;i++)
+{
+for(j=1;j<=i;j++)
+{
+System.out.print("*");
+
+}
+System.out.println("");
+
+
+
+}
+
+
+
+
+}
+
+
+
+
+
+}
